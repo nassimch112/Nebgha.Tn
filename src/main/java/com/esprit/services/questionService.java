24ba64b405288dbffad741feed0373fafb3b179d
nassimch112 <nassim.chakroun@esprit.tn>
2024-02-16 16:39:54 +0100

    models, service, data source

---
+++ src/main/java/com/esprit/services/questionService.java
@@ -0,0 +1,82 @@
+package com.esprit.services;
+
+import main.java.com.esprit.utils.DataSource;
+import com.esprit.models.question;
+
+import com.esprit.services.IService;
+
+import java.sql.*;
+import java.util.*;
+
+
+public class questionService implements IService<question> {
+    private Connection connection;
+
+    public questionService() {
+        connection = DataSource.getInstance().getConnection();
+    }
+    @Override
+    public void ajouter(question question) {
+        String req = "INSERT into questions_forum(id, titre, auteur_id, date_creation, sujet_id, contenu) values (" + question.getId() +
+                ", '" + question.getTitre() + "', " + question.getAuteur_id() +
+                ", '" + question.getDate()  + "', " +
+                question.getSujet_id() + ", '" + question.getContenu() + "');";
+        try {
+            Statement st = connection.createStatement();
+            st.executeUpdate(req);
+            System.out.println("Question ajoutée !");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    //TODO: fix date input;
+    @Override
+    public void modifier(question question) {
+        String req = "UPDATE questions_forum set titre = '" + question.getTitre() +
+                "', auteur_id = " + question.getAuteur_id() +
+                ", date_creation = '" + question.getDate() +
+                "', sujet_id = " + question.getSujet_id() +
+                ", contenu = '" + question.getContenu() +
+                "' where id = " + question.getId() + ";";
+        try {
+            Statement st = connection.createStatement();
+            st.executeUpdate(req);
+            System.out.println("Question modifiée !");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Override
+    public void supprimer(question question) {
+        String req = "DELETE from questions_forum where id = " + question.getId() + ";";
+        try {
+            Statement st = connection.createStatement();
+            st.executeUpdate(req);
+            System.out.println("Question supprimée !");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Override
+    public List<question> afficher() {
+        List<question> questions = new ArrayList<>();
+
+        String req = "SELECT * from questions_forum";
+        try {
+            Statement st = connection.createStatement();
+            ResultSet rs = st.executeQuery(req);
+            while (rs.next()) {
+                questions.add(new question(rs.getInt("id"), rs.getString("titre"),
+                        rs.getInt("auteur_id"), rs.getDate("date_creation"),
+                        rs.getInt("sujet_id"), rs.getString("contenu")));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return questions;
+    }
+
+}
---
+++ src/main/java/com/esprit/services/reponseService.java
@@ -0,0 +1,81 @@
+package com.esprit.services;
+
+import main.java.com.esprit.utils.DataSource;
+import com.esprit.models.reponse;
+
+import com.esprit.services.IService;
+
+import java.sql.*;
+import java.util.*;
+
+public class reponseService implements IService<reponse> {
+
+
+    private Connection connection;
+
+    public reponseService() {
+        connection = DataSource.getInstance().getConnection();
+    }
+
+    @Override
+    public void ajouter(reponse reponse) {
+        String req = "INSERT into reponse_forum(auteur_id, question_id, contenu, date_creation, sujet_id) values ('" + reponse.getAuteur_id() + "', '"
+                + reponse.getQuestion_id() + "', '" + reponse.getContenu() + "', '" + reponse.getDate() +
+                "', "+ reponse.getSujet_id() + ");";
+        try {
+            Statement st = connection.createStatement();
+            st.executeUpdate(req);
+            System.out.println("Réponse ajoutée !");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Override
+    public void modifier(reponse reponse) {
+        String req = "UPDATE reponse_forum set auteur_id = '" + reponse.getAuteur_id() + "', question_id = '"
+                + reponse.getQuestion_id() + "', contenu = '" + reponse.getContenu()
+                + "', date_creation = '" + reponse.getDate() + "', sujet_id = " + reponse.getSujet_id() +
+                " where id = " + reponse.getId() + ";";
+        try {
+            Statement st = connection.createStatement();
+            st.executeUpdate(req);
+            System.out.println("Réponse modifiée !");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Override
+    public void supprimer(reponse reponse) {
+        String req = "DELETE from reponse_forum where id = " + reponse.getId() + ";";
+        try {
+            Statement st = connection.createStatement();
+            st.executeUpdate(req);
+            System.out.println("réponse supprimée !");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Override
+    public List<reponse> afficher() {
+        List<reponse> reponses = new ArrayList<>();
+
+        String req = "SELECT * from reponse_forum";
+        try {
+            Statement st = connection.createStatement();
+            ResultSet rs = st.executeQuery(req);
+            while (rs.next()) {
+                reponses.add(new reponse(rs.getInt("id"), rs.getInt("auteur_id"),
+                        rs.getInt("question_id"), rs.getString("contenu"),
+                        rs.getDate("date_creation"), rs.getInt("sujet_id")));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return reponses;
+    }
+
+}
